9ec23de4b28569a6c388f6a969070a2cfbb112b3
unknown <kgeluza@mail.ru>
2021-11-17 02:41:52 +0400

    commit1

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,50 @@
+import Team from '../app';
+
+test('проверка метода add', () => {
+  const team = new Team();
+  const character = {
+    name: 'Alex', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  };
+  team.add(character);
+  expect(team.members).toContain(character);
+});
+
+test('проверка метода add на выброс ошибки при дублировании', () => {
+  const team = new Team();
+  const character = {
+    name: 'Alex', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  };
+  team.add(character);
+  expect(() => team.add(character)).toThrowError();
+});
+
+test('проверка метода addAll', () => {
+  const team = new Team();
+  const bowerman = {
+    name: 'Alex', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  };
+  const zombie = {
+    name: 'Svyatoslav', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10,
+  };
+  const daemon = {
+    name: 'Di', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40,
+  };
+  team.addAll(bowerman, zombie, daemon, zombie);
+  expect(team.members).toContain(bowerman, zombie, daemon);
+});
+
+test('проверка метода toArray', () => {
+  const team = new Team();
+  const bowerman = {
+    name: 'Alex', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+  };
+  const zombie = {
+    name: 'Svyatoslav', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10,
+  };
+  const daemon = {
+    name: 'Di', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40,
+  };
+  team.addAll(bowerman, zombie, daemon);
+  team.toArray();
+  expect(team.members).toEqual([bowerman, zombie, daemon]);
+});
---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+// TODO: write your code here
+
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Персонаж character уже существует!');
+    } else {
+      this.members.add(character);
+    }
+  }
+
+  addAll(...character) {
+    character.forEach((item) => this.members.add(item));
+  }
+
+  toArray() {
+    this.members = Array.from(this.members);
+  }
+}
